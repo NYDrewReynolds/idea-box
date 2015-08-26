26b4d882ca187879126e6406a4ceb13d5ab844bb
Drew Reynolds <NYDrewReynolds@gmail.com>
2015-08-26 15:43:40 -0600

    Fix AJAX delete, reinstall Responders

---
+++ app/assets/javascripts/create_and_remove_idea.js
@@ -0,0 +1,47 @@
+function submitIdea(event) {
+    event.preventDefault();
+    postIdeaData();
+}
+
+function ideaData() {
+    return {
+        idea: {
+            title: $("#idea-title").val(),
+            body: $("#idea-body").val()
+        }
+    };
+}
+
+function postIdeaData() {
+    $.post("/ideas", ideaData(), function(data){
+        $("#ideas").append(data);
+        $ideaDiv = $(data);
+        $('#ideas').last().click(deleteIdea);
+    });
+    $("#new_idea")[0].reset();
+}
+
+function deleteIdea() {
+  $('body').on('click', '.remove-idea',function(event){
+    event.preventDefault();
+    var idea = $(this).closest('.panel');
+    var ideaId = $(this).closest('.panel').data().id;
+
+    $.ajax({
+      method: "DELETE",
+      url: "/ideas/" + ideaId,
+      data: { id: ideaId },
+      success: function () {
+        idea.remove();
+      },
+      error: function (xhr, ajaxOptions, thrownError) {
+        console.log(xhr.status);
+      }
+    });
+  });
+}
+
+$(document).ready(function () {
+    $("#new_idea").submit(submitIdea);
+    deleteIdea();
+});
--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,11 +1,23 @@
 class IdeasController < ApplicationController
   before_action :set_ideas, only: [:index, :create]
 
+  def index
+    @idea = Idea.new
+  end
+
   def create
     idea = Idea.new(idea_params)
     idea.quality = "swill"
     idea.save
     render partial: "shared/idea", locals: {idea: idea}
+  end
+
+  def destroy
+    @idea = Idea.find(params[:id])
+    @idea.destroy
+    respond_to do |format|
+      format.js { render nothing: true }
+    end
   end
 
   private
@@ -16,7 +28,6 @@ class IdeasController < ApplicationController
 
   def set_ideas
     @ideas = Idea.all
-    @idea = Idea.new
   end
 
 end
